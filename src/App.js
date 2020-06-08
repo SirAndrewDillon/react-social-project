d63cad2521256de709f31b684739ef602cbf32d4
Andrew Dillon <majik895@gmail.com>
2020-06-08 10:38:58 -0400

    more css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
 import  Users from './user/pages/Users'
 import NewPlace from './places/pages/NewPlace'
+import MainNavigation from './shared/components/Navigation/MainNavigation'
 
 
 const App = () => {
   return <Router>
+    <MainNavigation />
+    <main>
     <Switch>
     <Route path="/" exact>
       <Users/>
@@ -14,7 +17,8 @@ const App = () => {
       <NewPlace/>
     </Route>
     <Redirect to="/" />
-    </Switch>
+      </Switch>
+      </main>
   </Router>
 }
 
