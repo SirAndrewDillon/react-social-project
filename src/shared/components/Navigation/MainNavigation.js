b15195c0d853b268acfe95179b06c91d7b557288
Andrew Dillon <majik895@gmail.com>
2020-06-08 11:49:00 -0400

    drawer fully functional

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,44 +1,46 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
-import SideDrawer from './SideDrawer'
+import SideDrawer from './SideDrawer';
 import './MainNavigation.css';
 import Backdrop from '../IUElements/Backdrop';
 
 const MainNavigation = (props) => {
-  const [drawerIsOpen, setDrawerIsOpen] = useState(false)
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-  const openDrawer = () => {
-    setDrawerIsOpen(true)
-  }
-  
-  const closeDrawer = () => {
-    setDrawerIsOpen(false)
-  }
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
 
   return (
     <React.Fragment>
-      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
-      { drawerIsOpen && <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+        <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+          <nav className='main-navigation__drawer-nav'>
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+    
+      <MainHeader>
+    
+        <button className='main-navigation__menu-btn' onClick={openDrawerHandler}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className='main-navigation__title'>
+          <Link to='/'>My Places</Link>
+        </h1>
+        <nav className='main-navigation__header-nav'>
           <NavLinks />
         </nav>
-      </SideDrawer>}
-    <MainHeader>
-      <button className='main-navigation__menu-btn' onClick={openDrawer}>
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className='main-navigation__title'>
-        <Link to='/'>My Places</Link>
-      </h1>
-      <nav className="main-navigation__header-nav">
-        <NavLinks />
-      </nav>
       </MainHeader>
-      </React.Fragment>
+    </React.Fragment>
   );
 };
 
